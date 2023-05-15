dd30d072def993be3f116a6b60e9e8876d822bef
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-05-15 12:50:13 +0400

    minoer improvements

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,36 +10,44 @@ interface Messages {
 }
 
 export const getMessageData = (): Promise<Messages> => {
-  return axios("http://localhost:5000")
-    .then(({ data }) => ({
-      lastHourMessages: data.lastHourMessages
-        .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
-        .map((message: BaseMessage) => ({
-          ...message,
-          status: MessageStatus.PENDING,
-          processStatus: ProcessStatus.PENDING,
-        })),
-      lastFourHourMessages: data.lastFourHourMessages
-        .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
-        .map((message: BaseMessage) => ({
-          ...message,
-          status: MessageStatus.PENDING,
-          processStatus: ProcessStatus.PENDING,
-        })),
-      lastDayMessages: data.lastDayMessages
-        .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
-        .map((message: BaseMessage) => ({
-          ...message,
-          status: MessageStatus.PENDING,
-          processStatus: ProcessStatus.PENDING,
-        })),
-      olderMessages: data.olderMessages
-        .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
-        .map((message: BaseMessage) => ({
-          ...message,
-          status: MessageStatus.PENDING,
-          processStatus: ProcessStatus.PENDING,
-        })),
-    }))
-    .catch();
+  if(process.env.API_URL) {
+    return axios(process.env.API_URL)
+      .then(({ data }) => ({
+        lastHourMessages: data.lastHourMessages
+          .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
+          .map((message: BaseMessage) => ({
+            ...message,
+            status: MessageStatus.PENDING,
+            processStatus: ProcessStatus.PENDING,
+          })),
+        lastFourHourMessages: data.lastFourHourMessages
+          .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
+          .map((message: BaseMessage) => ({
+            ...message,
+            status: MessageStatus.PENDING,
+            processStatus: ProcessStatus.PENDING,
+          })),
+        lastDayMessages: data.lastDayMessages
+          .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
+          .map((message: BaseMessage) => ({
+            ...message,
+            status: MessageStatus.PENDING,
+            processStatus: ProcessStatus.PENDING,
+          })),
+        olderMessages: data.olderMessages
+          .sort((a: BaseMessage, b: BaseMessage) => b.date - a.date)
+          .map((message: BaseMessage) => ({
+            ...message,
+            status: MessageStatus.PENDING,
+            processStatus: ProcessStatus.PENDING,
+          })),
+      }))
+      .catch();
+  }
+  return Promise.resolve({
+    lastHourMessages: [],
+    lastFourHourMessages: [],
+    lastDayMessages: [],
+    olderMessages: [],
+  })
 };
