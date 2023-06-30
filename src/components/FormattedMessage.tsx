0fae7e07e894c580f916d58b4c138578f0d67eae
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-06-30 19:59:03 +0400

    full copy search

--- src/components/FormattedMessage.tsx
+++ src/components/FormattedMessage.tsx
@@ -6,20 +6,53 @@ import {
   CopyAll,
   QuestionMark,
 } from "@mui/icons-material";
-import { Card, IconButton, Link } from "@mui/material";
+import { Box, Card, IconButton, Link, Modal, Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import { supabase } from "../db";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { MessageStatus } from "@/constants";
 import { StatusTooltip } from "./StatusButton";
 import { parseEntities } from "@/parseEntities";
+import { DuplicateMessages } from "./DuplicateMessages";
+import { parseLoadedMessage } from "@/utils";
 
 interface IProps {
   message: IMessage;
+  ignoreDuplicates?: boolean;
+  onStatusChange?: () => void;
 }
 
-export const FormattedMessage = ({ message }: IProps) => {
+
+export const FormattedMessage = ({ message, ignoreDuplicates, onStatusChange }: IProps) => {
+  const [open, setOpen] = useState(false);
+  const [copyMessages, setCopyMessages] = useState<{
+    [x: string]: any;
+}[] | null>(null)
   const isProcessable = message.status === MessageStatus.APPROVED || message.status === MessageStatus.INTERESTING
+  const baseRequest = useCallback(async (status: MessageStatus) => {
+    await supabase.from("messages").upsert({
+      tg_message_id: message.messageId,
+      tg_chat_name: message.chatName,
+      message_date: message.date,
+      text: message.text,
+      status,
+    });
+  }, [message])
+
+  const handleProcessable = useCallback(async (status: MessageStatus) => {
+    if (ignoreDuplicates) {
+      await baseRequest(status)
+    } else {
+      const { data } = await supabase.from("messages").select().eq('text', message.text).in('status', [MessageStatus.INTERESTING, MessageStatus.APPROVED])
+      if (!data?.length) {
+        await baseRequest(status)
+  
+      } else {
+        setCopyMessages(data)
+        setOpen(true)
+      }
+    }
+  }, [message, ignoreDuplicates, baseRequest])
   return (
     <Card>
       <div style={{ display: "flex", flexDirection: "column", padding: 12 }}>
@@ -40,14 +73,9 @@ export const FormattedMessage = ({ message }: IProps) => {
         <div>
           <StatusTooltip title="INTERESTING">
             <IconButton
-              onClick={async () => {
-                await supabase.from("messages").upsert({
-                  tg_message_id: message.messageId,
-                  tg_chat_name: message.chatName,
-                  message_date: message.date,
-                  text: message.text,
-                  status: MessageStatus.APPROVED,
-                });
+              onClick={() => {
+                handleProcessable(MessageStatus.APPROVED);
+                if(onStatusChange) onStatusChange()
               }}
             >
               <Check color="success" />
@@ -55,14 +83,9 @@ export const FormattedMessage = ({ message }: IProps) => {
           </StatusTooltip>
           <StatusTooltip title="POTENTIAL">
             <IconButton
-              onClick={async () => {
-                await supabase.from("messages").upsert({
-                  tg_message_id: message.messageId,
-                  tg_chat_name: message.chatName,
-                  message_date: message.date,
-                  text: message.text,
-                  status: MessageStatus.INTERESTING,
-                });
+              onClick={() => {
+                handleProcessable(MessageStatus.INTERESTING);
+                if(onStatusChange) onStatusChange()
               }}
             >
               <QuestionMark />
@@ -78,6 +101,7 @@ export const FormattedMessage = ({ message }: IProps) => {
                   text: message.text,
                   status: MessageStatus.REJECTED,
                 });
+                if(onStatusChange) onStatusChange()
               }}
             >
               <Close color="error" />
@@ -93,6 +117,7 @@ export const FormattedMessage = ({ message }: IProps) => {
                   text: message.text,
                   status: MessageStatus.COPY,
                 });
+                if(onStatusChange) onStatusChange()
               }}
             >
               <CopyAll color="warning" />
@@ -111,6 +136,7 @@ export const FormattedMessage = ({ message }: IProps) => {
                     text: message.text,
                     processed_at: dayjs(),
                   });
+                  if(onStatusChange) onStatusChange()
                 }}
               >
                 <ArchiveOutlined color="primary" />
@@ -119,6 +145,22 @@ export const FormattedMessage = ({ message }: IProps) => {
           ) : null}
         </div>
       </div>
+      <DuplicateMessages open={open} handleClose={() => setOpen(false)}>
+        {copyMessages ? (
+          <Stack direction='row' columnGap={2}>
+            <Stack rowGap={1}>
+              <Typography variant="h6">Сообщение на рассмотрении</Typography>
+              <FormattedMessage message={message} ignoreDuplicates onStatusChange={() => setOpen(false)} />
+            </Stack>
+            <Stack rowGap={1}>
+              <Typography variant="h6">Потенциальные копии</Typography>
+              {copyMessages.map((msg, i) => (
+                <FormattedMessage key={i} message={parseLoadedMessage(msg)} ignoreDuplicates onStatusChange={() => setOpen(false)} />
+              ))}
+            </Stack>
+          </Stack>
+        ) : null}
+      </DuplicateMessages>
     </Card>
   );
 };
